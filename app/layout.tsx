3f03ac92f62878babdd0af1cabfa7692ec31bf29
gggiyer812 <ganesh.iyer812@gmail.com>
2026-03-06 20:33:12 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     siteName: "ganeshiyer.ai",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://ganeshiyer.ai/",
         width: 1920,
         height: 1080,
         alt: "Ganesh Iyer",
